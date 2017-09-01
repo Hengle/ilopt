f8d74859a3daa9b44512a83e2e38eaf0e186b190
Tanner Gooding <tagoo@outlook.com>
2017-09-01 08:19:04 -0700

    ilopt.Says("Hello, World");

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# ilopt
+A tool for performing post-compilation optimization on managed assemblies. 
+
+## License
+Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See [License.md](License.md) in the repository root for more information.
---
+++ Sources/IlOptimizer/Program.cs
@@ -0,0 +1,16 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+namespace IlOptimizer
+{
+    /// <summary>Provides a set of methods for executing a program.</summary>
+    public static class Program
+    {
+        /// <summary>The entry point of the program.</summary>
+        /// <param name="args">The arguments that should be used when running the program.</param>
+        /// <returns>The exit code of the program.</returns>
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
